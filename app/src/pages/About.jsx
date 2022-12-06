5210f0abc7f47d68d4be1ca624436d7d956a8158
gabrielMatosBoubee <gm.boubee@gmail.com>
2022-12-06 10:58:18 -0300

    modifiquei o packge.json

--- app/src/pages/About.jsx
+++ app/src/pages/About.jsx
@@ -11,9 +11,9 @@ function About () {
     <Header />
     <main className="About">
       <aside className="AboutText">
-    <h1 style={{ 'marginTop': '10px'}}>Whos is I?</h1>
-    <p style={{ 'marginTop': '10px'}}>I am a person who want challenges, the one whose'll undestand the pain of the client
-      and become the solucion on codes! But now I'll tell you my history. I start to learn
+    <h1 style={{ 'marginTop': '10px'}}>Whos am I?</h1>
+    <p style={{ 'marginTop': '10px'}}>I am a person who wants challenges, the one who will undestand the client's disconfort
+      and bring the solucion on codes! But now I'll tell you my history. I start to learn
       progamation because I love technology and learn every day, so I decide to enter on trybe.
     </p>
     <h2 style={{ 'marginTop': '10px'}}>What I learn on trybe?</h2>
